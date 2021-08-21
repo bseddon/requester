76dd89e68fbc29e7741e543abaa8aa6f17210993
bseddon <bill.seddon@lyquidity.com>
2021-08-21 00:58:21 +0000

    Initial commit

---
+++ src/OCSP/Exception/VerificationException.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Originally from https://github.com/mlocati/ocsp
+ * Changed namespaces so there are no clashes with the orginal project
+ */
+
+namespace lyquidity\OCSP\Exception;
+
+/**
+ * Exception thrown when the response is signed but the OCSP signature cannot be verified.
+ */
+abstract class VerificationException extends Exception
+{
+}
