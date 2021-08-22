31acceabc50c50ed574c51ac4665a1d663e8a300
bseddon <bill.seddon@lyquidity.com>
2021-08-22 20:39:55 +0000

    Initial commit of a TSA specific exception class

---
+++ src/TSA/TSAException.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Exception handler for TSA support.
+ * 
+ * Copyright © 2021 Lyquidity Solutions Limited
+ * License GPL 3.0.0
+ * Bill Seddon 2021-08-22
+ */
+
+namespace lyquidity\TSA;
+
+use lyquidity\OCSP\Exception\Exception;
+
+class TSAException extends Exception
+{}
