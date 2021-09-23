8e5f21c05a590472ce4b24585d5c91b798931099
bseddon <bill.seddon@lyquidity.com>
2021-09-23 22:45:10 +0000

    Changed to accommodate Linux

--- src/autoload.php
+++ src/autoload.php
@@ -5,6 +5,7 @@ function requestor_autoload( $classname )
 	$prefix = 'lyquidity\\';
 	if ( strpos( $classname, $prefix ) !== 0 ) return false;
 	$filename = substr( $classname, strlen( $prefix ) ) . '.php';
+	$filename = str_replace('\\', '/', $filename);
 	if ( ! file_exists(  __DIR__ . "/$filename" ) ) return false;
 	// $filename = "$classname.php";
 	require_once  __DIR__ . "/$filename" ;
